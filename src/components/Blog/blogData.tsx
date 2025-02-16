b2c4437eb94c10ba9ecb34fee41b11d26fbd36b1
DuyDQHE163434 <133103039+DuyDQHE163434@users.noreply.github.com>
2025-02-16 08:39:41 +0700

    update blog and blog details

---
+++ src/components/Blog/blogData.tsx
@@ -0,0 +1,47 @@
+import { Blog } from "@/types/blog";
+
+const blogData: Blog[] = [
+  {
+    id: 1,
+    title: "Mazda 3 1.5AT 2019",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+    image: "https://cms.anycar.vn/wp-content/uploads/2024/01/1be0dc42-20240122_072630-711x533.jpg?_t=2025021014",
+    author: {
+      name: "Samuyl Joshi",
+      image: "/images/blog/author-01.png",
+      designation: "Graphic Designer",
+    },
+    tags: [""],
+    publishDate: "2025",
+  },
+  {
+    id: 2,
+    title: "Xe Peugeot 3008 1.6AT 2019",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+    image: "https://cms.anycar.vn/wp-content/uploads/2024/01/5c989917-20240110_082021-711x533.jpg?_t=2025021014",
+    author: {
+      name: "Musharof Chy",
+      image: "/images/blog/author-02.png",
+      designation: "Content Writer",
+    },
+    tags: [""],
+    publishDate: "2025",
+  },
+  {
+    id: 3,
+    title: "Ford Focus Hatchback 1.8AT 2012",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+    image: "https://cms.anycar.vn/wp-content/uploads/2024/01/ec1871ab-20240103_043317-646x533.jpg?_t=2025021014",
+    author: {
+      name: "Lethium Deo",
+      image: "/images/blog/author-03.png",
+      designation: "Graphic Designer",
+    },
+    tags: [""],
+    publishDate: "2025",
+  },
+];
+export default blogData;
