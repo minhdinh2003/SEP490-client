7285bb89cb4dd672b0328c72c2411a3731622e13
Linhlon1904 <linhdead1904@gmail.com>
2025-03-04 11:59:43 +0700

    fix contact and product

---
+++ src/components/Features/featuresData.tsx
@@ -0,0 +1,187 @@
+import { Feature } from "@/types/feature";
+import Image from "next/image";
+import Link from "next/link";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    bgImage: (
+      <Link
+        href="/"
+        className={`header-logo block w-full`}
+      >
+        <Image
+          src="https://cms.anycar.vn/wp-content/uploads/2022/10/3dd8a08b-20221026_090134-1.jpg"
+          alt="logo"
+          width={120}
+          height={70}
+          className="w-full dark:hidden"
+        />
+
+      </Link>
+    ),
+  },
+  {
+    id: 1,
+    bgImage: (
+      <Link
+        href="/"
+        className={`header-logo block w-full`}
+      >
+        <Image
+          src="https://cms.anycar.vn/wp-content/uploads/2022/10/3dd8a08b-20221026_090134.jpg"
+          alt="logo"
+          width={120}
+          height={70}
+          className="w-full dark:hidden"
+        />
+
+      </Link>
+    ),
+  },
+  {
+    id: 1,
+    bgImage: (
+      <Link
+        href="/"
+        className={`header-logo block w-full`}
+      >
+        <Image
+          src="https://cms.anycar.vn/wp-content/uploads/2022/10/1e745c07-20221026_091317.jpg"
+          alt="logo"
+          width={120}
+          height={70}
+          className="w-full dark:hidden"
+        />
+
+      </Link>
+    ),
+  },
+  {
+    id: 1,
+    bgImage: (
+      <Link
+        href="/"
+        className={`header-logo block w-full`}
+      >
+        <Image
+          src="https://cms.anycar.vn/wp-content/uploads/2022/10/312f6f4b-20221026_090129-1.jpg"
+          alt="logo"
+          width={120}
+          height={70}
+          className="w-full dark:hidden"
+        />
+
+      </Link>
+    ),
+  },
+  {
+    id: 1,
+    bgImage: (
+      <Link
+        href="/"
+        className={`header-logo block w-full`}
+      >
+        <Image
+          src="https://cms.anycar.vn/wp-content/uploads/2022/10/d84e5480-20221026_090133-1.jpg"
+          alt="logo"
+          width={120}
+          height={70}
+          className="w-full dark:hidden"
+        />
+
+      </Link>
+    ),
+  },
+  {
+    id: 1,
+    bgImage: (
+      <Link
+        href="/"
+        className={`header-logo block w-full`}
+      >
+        <Image
+          src="https://cms.anycar.vn/wp-content/uploads/2022/10/426c5bec-20221026_090132-1.jpg"
+          alt="logo"
+          width={120}
+          height={70}
+          className="w-full dark:hidden"
+        />
+
+      </Link>
+    ),
+  },
+  {
+    id: 1,
+    bgImage: (
+      <Link
+        href="/"
+        className={`header-logo block w-full`}
+      >
+        <Image
+          src="https://cms.anycar.vn/wp-content/uploads/2022/10/7005a711-20221026_090136.jpg"
+          alt="logo"
+          width={120}
+          height={70}
+          className="w-full dark:hidden"
+        />
+
+      </Link>
+    ),
+  },
+  {
+    id: 1,
+    bgImage: (
+      <Link
+        href="/"
+        className={`header-logo block w-full`}
+      >
+        <Image
+          src="https://cms.anycar.vn/wp-content/uploads/2022/10/1e745c07-20221026_091317.jpg"
+          alt="logo"
+          width={120}
+          height={70}
+          className="w-full dark:hidden"
+        />
+
+      </Link>
+    ),
+  },
+  {
+    id: 1,
+    bgImage: (
+      <Link
+        href="/"
+        className={`header-logo block w-full`}
+      >
+        <Image
+          src="https://cms.anycar.vn/wp-content/uploads/2022/10/312f6f4b-20221026_090129-1.jpg"
+          alt="logo"
+          width={120}
+          height={70}
+          className="w-full dark:hidden"
+        />
+
+      </Link>
+    ),
+  },
+  {
+    id: 1,
+    bgImage: (
+      <Link
+        href="/"
+        className={`header-logo block w-full`}
+      >
+        <Image
+          src="https://cms.anycar.vn/wp-content/uploads/2022/10/d84e5480-20221026_090133-1.jpg"
+          alt="logo"
+          width={120}
+          height={70}
+          className="w-full dark:hidden"
+        />
+
+      </Link>
+    ),
+  },
+];
+export default featuresData;
