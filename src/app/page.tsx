15e1f118eda25f66d69ab4d4fe9ba3eebbacc6d0
DuyDQHE163434 <133103039+DuyDQHE163434@users.noreply.github.com>
2025-03-19 17:45:15 +0700

    update app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,11 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Blog from "@/components/Blog";
-import Brands from "@/components/Brands";
-import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
-import Features from "@/components/Features";
-import Introduce from "@/components/Introduce";
-import Testimonials from "@/components/Testimonials";
-import { Metadata } from "next";
-import Carousel from "@/components/Carousel";
-export const metadata: Metadata = {
-  title: "Mua bán, sửa chữa xe",
-  description: "Website chuyển cung cấp dịch vụ mua bán, sửa chữa xe",
-};
+import PageHome2 from "./home-2/page";
 
-export default function Home() {
+function PageHome() {
   return (
-    <>
-      <ScrollUp />
-      <Introduce />
-      <Carousel />
-      <Features />
-      <AboutSectionOne />
-      <AboutSectionTwo />
-      <Brands />
-      <Testimonials />
-      {/* <Pricing /> */}
-      <Blog />
-      {/* <Contact /> */}
-    </>
+    <div className="relative overflow-hidden nc-PageHome">
+      <PageHome2 />
+    </div>
   );
 }
+
+export default PageHome;
