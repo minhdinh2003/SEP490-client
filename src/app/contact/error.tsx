fea5f5d20a5645f845f9f54af0762b4e0fe20b67
DucTV <ductvhe153308@fpt.edu.vn>
2025-04-07 23:35:15 +0700

    Contact error page

---
+++ src/app/contact/error.tsx
@@ -0,0 +1,31 @@
+'use client' // Error components must be Client Components
+ 
+import { useEffect } from 'react'
+ 
+export default function ContactError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+ 
+  return (
+    <div className='flex flex-col items-center justify-center py-[100px]'>
+      <h2>Something went wrong!</h2>
+      <button
+      className='p-2 bg-black text-[white] cursor-pointer rounded-md mt-3'
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  )
+}
