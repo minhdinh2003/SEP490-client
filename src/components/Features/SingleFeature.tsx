8260b1df8358bb1f29e84adfbcff62d04ee32e33
minhdinh2003 <minhddhe176148@fpt.edu.vn>
2025-04-10 21:56:57 +0700

    product-detail fix create method

--- src/components/Features/SingleFeature.tsx
+++ src/components/Features/SingleFeature.tsx
@@ -1,12 +1,24 @@
-import { Feature } from "@/types/feature";
-
-const SingleFeature = ({ feature }: { feature: Feature }) => {
-  const { bgImage, title, paragraph } = feature;
+import { Brand } from "@/type/brand";
+import Image from "next/image";
+import Link from "next/link";
+const SingleFeature = ({ data }: { data: Brand }) => {
+  const { logoURL } = data;
   return (
     <div className="w-full">
-      <div className="wow fadeInUp items-center justify-center flex" data-wow-delay=".15s">
+      <div
+        className="wow fadeInUp items-center justify-center flex"
+        data-wow-delay=".15s"
+      >
         <div className="cursor-pointer mb-2 flex h-[100px] w-[100px] items-center justify-center rounded-md text-primary">
-          {bgImage}
+          <Link href={`/collection?brand=${data.id}`} className={`header-logo block w-full`} >
+            <Image
+              src={logoURL}
+              alt="logo"
+              width={120}
+              height={70}
+              className="w-full dark:hidden"
+            />
+          </Link>
         </div>
       </div>
     </div>
--- src/components/Features/featuresData.tsx
+++ src/components/Features/featuresData.tsx
@@ -1,4 +1,4 @@
-import { Feature } from "@/types/feature";
+import { Feature } from "@/type/feature";
 import Image from "next/image";
 import Link from "next/link";
 
