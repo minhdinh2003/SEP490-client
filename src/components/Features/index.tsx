81ebf3fae38765e81450086fc14ff5214fcfb9db
DuyDQHE163434 <133103039+DuyDQHE163434@users.noreply.github.com>
2025-04-16 17:18:27 +0700

    update fearures

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -1,8 +1,12 @@
+import { Brand } from "@/type/brand";
 import SectionTitle from "../Common/SectionTitle";
 import SingleFeature from "./SingleFeature";
 import featuresData from "./featuresData";
-
-const Features = () => {
+import React, { FC } from "react";
+export interface DataProps {
+  brands: Brand[];
+}
+const Features  :FC<DataProps> = ({brands}) => {
   return (
     <>
       <section id="features" className="py-8 md:py-10 lg:py-14">
@@ -15,8 +19,8 @@ const Features = () => {
           />
 
           <div className="grid grid-cols-1 gap-x-4 gap-y-7 md:grid-cols-2 lg:grid-cols-3">
-            {featuresData.map((feature) => (
-              <SingleFeature key={feature.id} feature={feature} />
+            {brands.map((feature) => (
+              <SingleFeature key={feature.id} data={feature} />
             ))}
           </div>
         </div>
