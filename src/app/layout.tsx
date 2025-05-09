cdb3a84561bb0a346e89adc793375bc99c83fd75
DuyDQHE163434 <133103039+DuyDQHE163434@users.noreply.github.com>
2025-05-09 07:57:15 +0700

    fix layout app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,24 +16,19 @@ import { io } from "socket.io-client";
 import { v4 as uuidv4 } from "uuid";
 import CommonClient from "./CommonClient";
 import "./globals.scss";
-import { Roboto } from "next/font/google";
+import { Roboto, Roboto_Serif } from "next/font/google";
 import UserService from "@/http/userService";
 import { IPagingParam } from "@/contains/paging";
-const roboto = Roboto({
+import CustomerChat from "@/components/CustomerChat"; // Import component chat
+import OwnerChat from "@/components/OwnerChat";
+
+const roboto = Roboto_Serif({
   weight: ["400", "700", "500"],
-  // style: ['normal', 'italic'],
   subsets: ["vietnamese"],
-  // display: 'swap',
 });
 
 export let socket: any;
 export const connectionID = uuidv4();
-
-// const poppins = Poppins({
-//   subsets: ["latin"],
-//   display: "swap",
-//   weight: ["300", "400", "500", "600", "700"],
-// });
 
 function RootLayout({
   children,
@@ -58,6 +53,7 @@ function RootLayout({
       getListCart();
     }
   }, []);
+
   useEffect(() => {
     const param: IPagingParam = {
       pageSize: 1000,
@@ -79,10 +75,10 @@ function RootLayout({
       })
       .catch(() => {});
   }, [user]);
+
   const pathname = usePathname();
   const listNotFootter = ["/auction-detail"];
   const isNotFooter = listNotFootter.some((path) => pathname.startsWith(path));
-  // Noti
 
   const connectSocket = () => {
     const userID = user.id;
@@ -107,9 +103,9 @@ function RootLayout({
           console.log("Connected to server");
           socket.emit("register", userID, connectionID);
         });
-        // nhận thông báo từ ng dùng
+
+        // Nhận thông báo từ người dùng
         socket.on("message", (message: any) => {
-          //NotificationID
           const data = JSON.parse(message?.rawData || {});
           if (
             message.type == "PRODUCT_OWNER_CHAT_REQUEST" ||
@@ -119,6 +115,12 @@ function RootLayout({
               ...message,
               message: data.message,
             });
+          }
+          if (
+            message.type == "USER_CHAT_REQUEST" ||
+            message.type == "OWNER_CHAT_WITH_USER"
+          ) {
+            messStore?.(data.requestId);
           }
 
           if (
@@ -134,6 +136,7 @@ function RootLayout({
         console.error("Error registering user:", error);
       });
   };
+
   useEffect(() => {
     if (user && user.id) {
       connectSocket();
@@ -142,6 +145,7 @@ function RootLayout({
       socket?.disconnect();
     };
   }, [user]);
+  const isOwner = user?.role == "OWNER";
   return (
     <html lang="en" dir="" className={roboto.className}>
       <body
@@ -151,9 +155,15 @@ function RootLayout({
         <SiteHeader />
         <ToastProvider>{children}</ToastProvider>
         <CommonClient />
+
+        {/* Thêm component chat */}
+        {!isOwner && <CustomerChat />}
+        {isOwner && <OwnerChat />}
+
         {!isNotFooter && <Footer />}
       </body>
     </html>
   );
 }
+
 export default RootLayout;
