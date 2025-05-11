56c35697ba13ba47082e05bfd2cf3040304fcfc1
DuyDQHE163434 <133103039+DuyDQHE163434@users.noreply.github.com>
2025-05-12 00:20:22 +0700

    fix brands data

--- src/components/Brands/brandsData.tsx
+++ src/components/Brands/brandsData.tsx
@@ -16,7 +16,6 @@ const brandsData: Brand[] = [
     description: "Hỗ trợ trả góp thủ tục tốt nhất, trong thời gian nhanh nhất"
   },
   {
-    
     id: 2,
     name: "Tailgrids",
     image: "/images/brands/checks.png",
